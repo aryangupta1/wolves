ee5b225e179e2b1f308ad490f0a00f95d140ca7c
aryangupta1 <aryangupta1@outlook.com>
2023-11-24 16:56:26 +1100

    homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,23 @@
 "use client";
 import { useState } from 'react'
-import { StatisticsEntryForm } from './components/statisticsEntryForm';
+import { StatisticsEntryForm } from './components/statisticsEntryForm'
 import { StatisticsList } from './components/statisticsList'
+import HomePage from './pages/home'
 
 export default function Home() {
   // This component is now marked for client-side execution
 
   const [statsList, setStatsList] = useState([])
 
-  const addStats = (newStats: any) => {
-    setStatsList([...statsList, newStats])
-  };
+  // const addStats = (newStats: any) => {
+  //   setStatsList([...statsList, newStats])
+  // };
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-4">
-      <StatisticsEntryForm onAddStats={addStats} />
-      <StatisticsList statsList={statsList} />
+      {/* <StatisticsEntryForm onAddStats={addStats} />
+      <StatisticsList statsList={statsList} /> */}
+      <HomePage/>
     </main>
   )
 }
---
+++ app/pages/home.tsx
@@ -0,0 +1,40 @@
+import { Button, Container, Grid, Paper, Typography } from '@mui/material'
+import Image from 'next/image'
+import Link from 'next/link'
+
+export default function HomePage() {
+  return (
+    <Container maxWidth="lg">
+      <Grid container spacing={3} alignItems="center" justifyContent="center" style={{ minHeight: '100vh' }}>
+        <Grid item xs={12} md={6}>
+          <Typography variant="h3" component="h1" gutterBottom>
+            Welcome to the Wolves Futsal Club
+          </Typography>
+          <Typography variant="h5" gutterBottom>
+            Uniting Passion and Excellence on the Field
+          </Typography>
+          <Typography paragraph>
+            Dive into the thrilling world of futsal with the Wolves. Our team embodies the spirit of the game, combining skill, teamwork, and a relentless pursuit of victory. Join us as we embark on an exciting season, showcasing top-tier talent and heart-pounding matches.
+          </Typography>
+          <Link href="/stats" passHref>
+            <Button variant="contained" color="primary" size="large">
+              View Season Stats
+            </Button>
+          </Link>
+        </Grid>
+        <Grid item xs={12} md={6}>
+          <Paper elevation={5} style={{ padding: '2rem' }}>
+            {/* <Image
+              src="/futsal_team.jpg" // Replace with your image path
+              alt="Wolves Futsal Team"
+              layout="responsive"
+              width={500}
+              height={300}
+              objectFit="cover"
+            /> */}
+          </Paper>
+        </Grid>
+      </Grid>
+    </Container>
+  );
+}
